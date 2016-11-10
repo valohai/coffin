4450f12a30ce9d27b0fcf5afe59bdb537c23f93d
Aarni Koskela <akx@iki.fi>
2016-11-10 18:09:42 +0200

    Fix imports in coffin/__init__.py

--- coffin/__init__.py
+++ coffin/__init__.py
@@ -1,4 +1,26 @@
 __version__ = (2, 0, '0')
 
-from common import *
-from static import *
+from coffin.common import (
+    LoadExtension,
+    URLExtension,
+    WithExtension,
+    SpacelessExtension,
+    load,
+    url,
+    with_,
+    spaceless,
+)
+
+from coffin.static import (
+    StaticExtension,
+    GetStaticPrefixExtension,
+    GetMediaPrefixExtension,
+)
+
+COFFIN_EXTENSIONS = [
+    'coffin.LoadExtension',
+    'coffin.URLExtension',
+    'coffin.WithExtension',
+    'coffin.SpacelessExtension',
+    'coffin.StaticExtension',
+]
