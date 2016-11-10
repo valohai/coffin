3350f7184043c9a4ba9a0af76ec27923feb2ca75
Aarni Koskela <akx@iki.fi>
2016-11-10 19:25:28 +0200

    Mark wheels universal and add Py2/Py3 trove classifiers

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ import re
 from setuptools import find_packages, setup
 
 here = os.path.dirname(os.path.abspath(__file__))
-version_re = re.compile(
-    r'__version__ = (\(.*?\))')
+version_re = re.compile(r'__version__ = (\(.*?\))')
 fp = open(os.path.join(here, 'coffin', '__init__.py'))
 version = None
 for line in fp:
@@ -15,13 +14,12 @@ for line in fp:
         version = eval(match.group(1))
         break
 else:
-    raise Exception("Cannot find version in __init__.py")
+    raise Exception('Cannot find version in __init__.py')
 fp.close()
 
-
-
-setup(name='Coffin',
-    version=".".join(map(str, version)),
+setup(
+    name='Coffin',
+    version='.'.join(map(str, version)),
     description='Jinja2 adapter for Django',
     author='Christopher D. Leary',
     author_email='cdleary@gmail.com',
@@ -30,10 +28,12 @@ setup(name='Coffin',
     url='http://github.com/coffin/coffin',
     packages=find_packages(exclude=('tests',)),
     classifiers=[
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development"
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
     ],
 )
